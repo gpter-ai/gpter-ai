6694bbeb9c7e2c75d47a39b6abf2dbdaed36d30d
Stanislav <stanislau.saprankou@gmail.com>
2023-04-15 19:07:08 +0200

    add prettier, change eslint config, run both and fix any issues

--- app/src/components/Content.tsx
+++ app/src/components/Content.tsx
@@ -5,40 +5,48 @@ import Container from '@cloudscape-design/components/container';
 import Header from '@cloudscape-design/components/header';
 import SpaceBetween from '@cloudscape-design/components/space-between';
 import Textarea from '@cloudscape-design/components/textarea';
-import { Box, Button, NonCancelableCustomEvent } from '@cloudscape-design/components';
+import {
+  Box,
+  Button,
+  NonCancelableCustomEvent,
+} from '@cloudscape-design/components';
 import { InputProps } from '@cloudscape-design/components/input/interfaces';
 import { useAutoGrowTextArea } from '@/hooks/useAutoGrowTextArea';
 
 export function Content() {
   const [text, setText] = useState('');
-  const {
-    containerRef,
-    updateTextAreaHeight,
-  } = useAutoGrowTextArea();
+  const { containerRef, updateTextAreaHeight } = useAutoGrowTextArea();
 
-  const onValueChange = (e: NonCancelableCustomEvent<InputProps.ChangeDetail>) => {
+  const onValueChange = (
+    e: NonCancelableCustomEvent<InputProps.ChangeDetail>,
+  ) => {
     setText(e.detail.value);
     updateTextAreaHeight();
   };
 
   return (
     <ContentLayout
-      header={(
+      header={
         <SpaceBetween size="m">
           <Header variant="h1">GTPer</Header>
         </SpaceBetween>
-            )}
+      }
     >
       <Container
-        header={(
+        header={
           <Header variant="h2" description="Please input your text">
             Generic assistant
           </Header>
-                )}
+        }
       >
         <div ref={containerRef}>
           <SpaceBetween size="m" direction="vertical">
-            <Textarea value={text} onChange={onValueChange} rows={1} autoFocus />
+            <Textarea
+              value={text}
+              onChange={onValueChange}
+              rows={1}
+              autoFocus
+            />
             <Box textAlign="right">
               <Button variant="primary">Submit</Button>
             </Box>
