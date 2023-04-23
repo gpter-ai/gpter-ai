944d60cd7e0b3153909f2001fec5b9e7c7274cb7
Dmitrii Babich <bbchd@amazon.com>
2023-04-23 17:44:10 +0200

    MockApiService implementation

--- app/src/api/mockApiService.ts
+++ app/src/api/mockApiService.ts
@@ -1,11 +1,52 @@
 import { ChatCompletionRequestMessage } from 'openai';
-import { ApiService } from './types';
+import {
+  ApiResponse,
+  ApiResponseConsumer,
+  ApiResponseType,
+  ApiService,
+} from './types';
+
+const SAMPLE_MESSAGE = ['Hello!', 'How', 'are', 'you', 'doing?'];
 
 export class MockApiService implements ApiService {
+  constructor(private apiResponseConsumer: ApiResponseConsumer) {}
+
   async sendMessages(
     messages: Array<ChatCompletionRequestMessage>,
   ): Promise<void> {
-    // eslint-disable-next-line no-console
-    console.log('MockApiService.sendMessages', messages);
+    let index = 0;
+    const intervalId = setInterval(() => {
+      if (index >= SAMPLE_MESSAGE.length) {
+        clearInterval(intervalId);
+        this.apiResponseConsumer.processResponse({
+          kind: ApiResponseType.Done,
+        });
+      } else {
+        this.apiResponseConsumer.processResponse(
+          this.createDataResponse(index),
+        );
+      }
+      index += 1;
+    }, 1000);
+  }
+
+  private createDataResponse(index: number): ApiResponse {
+    return {
+      kind: ApiResponseType.Data,
+      data: {
+        created: new Date().getDate(),
+        object: 'message',
+        model: 'Mock',
+        id: '123',
+        choices: [
+          {
+            message: {
+              content: SAMPLE_MESSAGE[index],
+              role: 'assistant',
+            },
+          },
+        ],
+      },
+    };
   }
 }
