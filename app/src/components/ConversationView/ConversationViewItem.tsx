47acd93eb2bbcb54f5aed2bd2ec6f0df87ed14e4
Stanislav <stanislau.saprankou@gmail.com>
2023-05-03 13:57:18 +0200

    wrap conversation view item in memo

--- app/src/components/ConversationView/ConversationViewItem.tsx
+++ app/src/components/ConversationView/ConversationViewItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { ChatItem, ChatItemType } from './types';
 import { MessageItem } from './MessageItem';
 import { SessionBreakItem } from '@/components/ConversationView/SessionBreakItem';
@@ -7,7 +7,23 @@ interface Props {
   content: ChatItem;
 }
 
-export const ConversationViewItem: React.FC<Props> = ({ content }) => {
+const shouldRerender = (
+  { content: prevContent }: Props,
+  { content: nextContent }: Props,
+): boolean => {
+  if (prevContent.type !== nextContent.type) {
+    return false;
+  }
+
+  if (prevContent.type === ChatItemType.Message) {
+    // @ts-ignore-line
+    return prevContent.message.content === nextContent.message.content;
+  }
+
+  return true;
+};
+
+export const ConversationViewItem: React.FC<Props> = memo(({ content }) => {
   switch (content.type) {
     case ChatItemType.Message:
       return <MessageItem message={content.message} />;
@@ -16,4 +32,4 @@ export const ConversationViewItem: React.FC<Props> = ({ content }) => {
   }
 
   throw new Error(`Unknown chat item type: ${JSON.stringify(content)}`);
-};
+}, shouldRerender);
