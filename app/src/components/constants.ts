18f95b0abcd63b2f175b40918e9a7cc4da747e38
Stanislav <stanislau.saprankou@gmail.com>
2023-05-12 21:42:50 +0200

    add language auto detection. POC of a different REGEX method

--- app/src/components/constants.ts
+++ app/src/components/constants.ts
@@ -2,6 +2,12 @@ export const OPENAI_API_KEY_PATTERN = /sk-\w{40,50}/;
 
 export const MAX_MESSAGES_PER_CHAT_PAGE = 5;
 
-export const CODE_BLOCK_DELIMITER = '```';
+export const CODE_BLOCK_DELIMITER = '\n```';
 
 export const FIRST_WORD_REGEX = /^([\w-]+)/;
+
+// if you want to have following groups after split: text -> lang -> code ...
+// export const CODE_LANGUAGE_REGEX = /```(?<language>[^\\ ]+)(?<code>.*?)```/;
+
+// export const CODE_REGEX = /```(?<code>((.|\n)*?))```/;
+// export const CODE_LANGUAGE_REGEX = /^(?<language>[^\\ ]+)/;
