74da2dbf1c01c13b646dc4718521ffa045077716
Stanislav <stanislau.saprankou@gmail.com>
2023-05-22 18:00:45 +0200

    change chat styles and icon

--- app/src/components/Chat.tsx
+++ app/src/components/Chat.tsx
@@ -4,6 +4,7 @@ import {
   Container,
   FormField,
   Header,
+  Icon,
   InputProps,
   NonCancelableCustomEvent,
   SpaceBetween,
@@ -145,23 +146,42 @@ const Chat: FC<object> = () => {
   };
 
   const SubmitButton = (
-    <Button
-      variant="primary"
-      iconName="angle-right"
-      iconAlign="right"
-      onClick={onMessageSubmit}
-    >
-      Submit
+    <Button variant="link" onClick={onMessageSubmit}>
+      <Box padding={{ bottom: 's' }}>
+        <Icon
+          svg={
+            <span>
+              <svg
+                className="message-submit__icon"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
+              </svg>
+            </span>
+          }
+          size="big"
+        />
+      </Box>
     </Button>
   );
 
   const StopButton = (
-    <Button
-      iconName="status-negative"
-      iconAlign="right"
-      onClick={onStopButtonClick}
-    >
-      Stop
+    <Button variant="link" onClick={onStopButtonClick}>
+      <Icon
+        svg={
+          <span>
+            <svg
+              className="message-stop__icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+          </span>
+        }
+        size="big"
+      />
     </Button>
   );
 
@@ -189,9 +209,11 @@ const Chat: FC<object> = () => {
       <div ref={containerRef}>
         <div className="header__line" />
         <ConversationView messages={history} />
-        <FormField errorText={inputError} stretch label="Type a query">
+        <hr />
+        <FormField errorText={inputError} stretch>
           <div className="chat__textarea-wrapper">
             <Textarea
+              placeholder="Write a message"
               onKeyDown={handleChatKeyDown}
               value={text}
               onChange={onValueChange}
