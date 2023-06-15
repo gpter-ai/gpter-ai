a18a35353f30cc271c7870e71af7854bd0c17b33
Stanislav <stanislau.saprankou@gmail.com>
2023-06-15 22:59:07 +0200

    refactor chatservice

--- app/src/types.ts
+++ app/src/types.ts
@@ -1,2 +1,8 @@
 export type Nullable<T> = T | null | undefined;
 export type NonNullable<T> = T extends null | undefined ? never : T;
+
+export type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
