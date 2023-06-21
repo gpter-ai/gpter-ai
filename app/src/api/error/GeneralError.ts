8d27b0660fa83e108ca8e01498e6eb9d0320f407
Stanislav <stanislau.saprankou@gmail.com>
2023-06-21 22:55:29 +0200

    add currency conversion function, fix error state in chat service

--- app/src/api/error/GeneralError.ts
+++ app/src/api/error/GeneralError.ts
@@ -4,7 +4,7 @@ export default class GeneralError extends ApiError {
   constructor(message?: string) {
     const reason =
       message ??
-      'Something went wrong when calling the assistant! Contact app developers for support if this error persists.';
+      'Something went wrong when calling the assistant! Try a different question or contact app developers for support if this error persists.';
 
     super(reason);
   }
