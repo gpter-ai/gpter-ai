a4dbcfe59eee96e91872d68c59b756babcfc3617
Stanislav <stanislau.saprankou@gmail.com>
2023-06-22 17:01:02 +0200

    add test for openAIService

--- app/src/api/openaiApiService.test.ts
+++ app/src/api/openaiApiService.test.ts
@@ -1,5 +1,38 @@
-describe('openApiService', () => {
-  it('should fetch messages', () => {
-    expect(true).toBe(true);
+import { IndexedStorageProvider } from '@/data/indexedDB/IndexedStorageProvider';
+import OpenAiApiService from './openaiApiService';
+import { API_TIMEOUT } from './constants';
+import TimeoutError from './error/TimeoutError';
+jest.mock('@/data/indexedDB/IndexedStorageProvider', () => ({
+  IndexedStorageProvider: jest.fn().mockImplementation(() => {
+    return {
+      getUserConfig: jest.fn().mockResolvedValue({ apiKey: 'test' }),
+    };
+  }),
+}));
+
+jest.mock('./constants', () => ({
+  API_TIMEOUT: 1,
+}));
+
+jest.mock('@microsoft/fetch-event-source', () => ({
+  fetchEventSource: () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(''), API_TIMEOUT * 100);
+    });
+  },
+}));
+
+describe('timeout', () => {
+  const instance = new OpenAiApiService(new IndexedStorageProvider());
+
+  test('throws TimeoutError if onopen is not called before API_TIMEOUT elapses', async () => {
+    await expect(() => instance.sendMessages([], () => {})).rejects.toThrow(
+      TimeoutError,
+    );
   });
 });
+
+// @TODO implement these tests
+describe('abort', () => {});
+
+describe('onmessage', () => {});
